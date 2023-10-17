61a590c9c2e2dc47c0c5a2ac21a19f7bc49676e8
DM <95344622+DM1009@users.noreply.github.com>
2023-10-17 16:58:10 +0900

    fix

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -10,7 +10,7 @@ export default function Map() {
   useEffect(() => {
     const mapScript = document.createElement('script')
     mapScript.async = true
-    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=62d4491f75cb223bfe5a91626c890790&autoload=false`
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=여기에 JS키를 적으세요0&autoload=false`
 
     document.head.appendChild(mapScript)
 
